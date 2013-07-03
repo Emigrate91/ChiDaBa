6281f7ca00babdd983b20a02678dbfec2d7c422a
Wraith <Wraith@10.55.101.33>
2013-07-03 21:11:10 +0200

    DB zu 1/5 kommentiert

--- src/DB/DB.java
+++ src/DB/DB.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package DB;
 
 import DataStructur.Art;
@@ -21,7 +17,10 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
-
+/**
+ * Caution! this claass contains everything Database related
+ * @author Team ChiDaBa
+ */
 public class DB {
         
     private Connection con = null;
@@ -31,7 +30,9 @@ public class DB {
     private boolean userValidity = false;
     
     private SimpleDateFormat dbFormat = new SimpleDateFormat("yyyy.MM.dd");
-    
+    /**
+     * collects and packs the information for the Database connection 
+     */
     private Connection ConnectDB () throws Exception {
         
         // Informationen zusammensuchen
@@ -57,7 +58,10 @@ public class DB {
         return con;
         
     }
-    
+    /**
+     * Closes the connection from the Database
+     * @throws SQLException if there is an SQL related error
+     */
     private void CloseDBConnection() throws SQLException {
         if (rslt != null) {
             rslt.close();
@@ -75,11 +79,15 @@ public class DB {
             pstmt.close();
         }
     }
-
+    /**
+     * get method of UserValidity
+     */
     public boolean getuserValidity() {
         return userValidity;
     }
-    
+    /**
+     * establishes an connection and creates the statments for the Database
+     */
     public ArrayList getPlantAttributes(Object PlantID) throws Exception{
         // Zur Datenbank verbinden
     con = ConnectDB();
@@ -135,7 +143,12 @@ public class DB {
         return null;
     }
 }
-    
+    /**
+     * establischen an DB-connection, collects the login data and closes the
+     * @param benutzername is used to identify the user
+     * @param passwort is used to validate the username
+     * @throws Exception dispays an error message 
+     */
     public void InsertIntoBenutzer (String benutzername, String passwort) throws Exception{
         
         try {
@@ -149,14 +162,14 @@ public class DB {
             pstmt.setString(3, passwort);
             pstmt.executeUpdate();
         }
-        catch (Exception e) {
-            
-        }
+        catch (Exception e) {System.err.println(e.getMessage());  }
         finally {
             this.CloseDBConnection();
         }
     }
-    
+    /**
+     * get method for the PlantID
+     */
     public Object getPlantID(Object sort, Object art) throws Exception
     {
     // Zur Datenbank verbinden
@@ -183,7 +196,9 @@ public class DB {
     else
         {return null;}
     }
-
+/**
+ * get method for TopfID
+ */
     public Object getTopfID(Object topfgr) throws Exception
     {
     // Zur Datenbank verbinden
@@ -209,7 +224,9 @@ public class DB {
         return null;
     }
     
-    
+    /**
+     * get method for Ereigniss_fk
+     */
     public Object getEreigniss_fk(Object PlantID) throws Exception
     {
     // Zur Datenbank verbinden
