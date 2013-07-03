5422de0ef98dd04a9d3d124df8698a26af21eadd
Wraith <Wraith@10.55.101.33>
2013-07-03 16:08:52 +0200

    Sorte is fertig kommentiert

--- src/DataStructur/Sorte.java
+++ src/DataStructur/Sorte.java
@@ -1,30 +1,32 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package DataStructur;
 
 import DB.DB;
 
 /**
- *
- * @author mn
+ * this class handles the object Sorte
+ * @author Team ChiDaBa
  */
 public class Sorte {
     private String sorte;
     private int reifezeit;
-
+    /**
+     * establishes an connection with the Database
+     */
     public Sorte(String sorte) throws Exception {
         DB con = new DB();
         this.sorte = sorte;
         this.reifezeit=con.getSorteReifezeit(sorte);
     }  
-    
+    /**
+     * fills the object Sorte
+     */
     public Sorte(String sorte, int reifezeit) {
         this.sorte = sorte;
         this.reifezeit = reifezeit;
     }
-    
+    /**
+     * get method of SorteID
+     */
     public int getSorteID() throws Exception{
         DB con = new DB();
         if(con.getList("sorte", "tbl_sorte").contains(this.getSorte())){
@@ -35,7 +37,9 @@ public class Sorte {
             return -1;
         }     
     }
-    
+    /**
+     * saves the object Sorte 
+     */    
     public void saveInDB() throws Exception{
          DB con = new DB();
          if(!con.getList("sorte", "tbl_sorte").contains(this.getSorte())){
@@ -48,19 +52,27 @@ public class Sorte {
          
     }
   
-    
+    /**
+     * get method of Sorte
+     */    
     public String getSorte() {
         return sorte;
     }
-
+    /**
+     * set method of Sorte
+     */
     public void setSorte(String sorte) {
         this.sorte = sorte;
     }
-
+    /**
+     * get method of Reifezeit
+     */
     public int getReifezeit() {
         return reifezeit;
     }
-
+    /**
+     * set emthod of Reifezeit
+     */
     public void setReifezeit(int reifezeit) {
         this.reifezeit = reifezeit;
     }
